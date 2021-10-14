d9be47545b89482bdcec4db90e7e94bf0271e77a
Vladimir <wlrybas@gmail.com>
2021-10-14 20:16:02 +0300

    функцию не успел доделать, в след PR все сдам

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-alert('This is great!')
+const player1 = {
+  name: 'player1',
+  hp: 400,
+  img: '',
+  weapon: ['fist'],
+  attack: function () {
+    console.log('this.name' + 'fight!')
+  }
+}
+
+const player2 = {
+  name: "player2",
+  hp: 400,
+  img: "",
+  weapon: ["wrench"],
+  attack: function () {
+    console.log("this.name" + "fight!");
+  },
+};
+
+function createPlayer(name, hp, image) {
+
+}
+
+createPlayer("player1", "SCORPION", 50);
+createPlayer("player2", "SUB-ZERO", 80);
