c7835c59aa05fa7d4abebf045dbbe3d43f5db151
Vladimir <wlrybas@gmail.com>
2021-10-28 21:57:59 +0300

    сделано на половину дз, в след PR доделаю

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ function renderHP() {
 const showResult = (name) => {
   const winTitle = createNewElement("div", "loseTitle");
   if (name) {
-    winTitle.innerText = name + " win!";
+    winTitle.innerText = name + " wins!";
   } else {
     winTitle.innerText = "Draw";
   }
@@ -222,10 +222,10 @@ const showWhoIsWin = () => {
 
   if (player1.hp === 0 && player1.hp < player2.hp) {
     arenas.appendChild(showResult(player2.name));
-    generateLogs("end", player1.name, player2.name);
+    generateLogs("end", player2, player1);
   } else if (player2.hp === 0 && player2.hp < player1.hp) {
     arenas.appendChild(showResult(player1.name));
-    generateLogs("end", player2.name, player1.name);
+    generateLogs("end", player1, player2);
   } else if (player2.hp === 0 && player1.hp === 0) {
     arenas.appendChild(showResult());
     generateLogs("draw");
